950b7193997c72a68d3025bbc1088479f735fb5b
Sneha <snehapowar580@gmail.com>
2025-11-09 19:38:50 +0530

    Updated .env and Banner.jsx for backend integration

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -17,13 +17,14 @@ const Banner = () => {
                 </div>
                 <button onClick={() => { navigate('/login'); scrollTo(0, 0) }} className='bg-white text-sm sm:text-base text-[#595959] px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all '>Create account</button>
             </div>
-
-            {/* ------- Right Side ------- */}
+{/*--- banner page ----*/}
+           
             <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
                 <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
             </div>
         </div>
     )
 }
+ 
 
 export default Banner
